7dc7b942792d5a0e61da9e2f2803e6c1a079fdca
Markus Thielker <mail.markus.thielker@gmail.com>
2023-05-03 21:44:51 +0200

    feat(svelte): add create/edit/remove to category

--- src/routes/categories/+page.server.ts
+++ src/routes/categories/+page.server.ts
@@ -1,5 +1,5 @@
-import type { PageServerLoad } from "./$types"
-import { redirect } from "@sveltejs/kit"
+import type { Actions, PageServerLoad } from "./$types"
+import { error, redirect } from "@sveltejs/kit"
 import { LOGIN_URL } from "$lib/constants"
 import { prismaClient } from "$lib/server/prisma"
 
@@ -17,5 +17,90 @@ export const load: PageServerLoad = async ({locals}) => {
     return {
         user,
         categories,
+    }
+}
+
+export const actions: Actions = {
+    create: async ({request, locals}) => {
+
+        const {user} = await locals.validateUser()
+        if (!user) throw redirect(302, LOGIN_URL)
+
+        const {
+            name,
+            color,
+        } = readFormData(await request.formData())
+
+        try {
+            await prismaClient.category.create({
+                data: {
+                    userId: user.userId,
+                    name,
+                    color,
+                },
+            })
+        } catch (e) {
+            throw error(400, "Invalid category data")
+        }
+
+        return
+    },
+    update: async ({request, locals}) => {
+
+        const {user} = await locals.validateUser()
+        if (!user) throw redirect(302, LOGIN_URL)
+
+        const formData = await request.formData()
+
+        const id = Number(formData.get("id") as string)
+        if (!id) return error(400, "Invalid category id")
+
+        const {
+            name,
+            color
+        } = readFormData(formData)
+
+        try {
+            await prismaClient.category.update({
+                where: {id},
+                data: {
+                    name,
+                    color,
+                },
+            })
+        } catch (e) {
+            return error(400, "Invalid category data")
+        }
+
+        return
+    },
+    remove: async ({request, locals}) => {
+
+        const {user} = await locals.validateUser()
+        if (!user) throw redirect(302, LOGIN_URL)
+
+        const formData = await request.formData()
+
+        const id = Number(formData.get("id") as string)
+        if (!id) return error(400, "Invalid payment id")
+
+        try {
+            await prismaClient.category.delete({where: {id}})
+        } catch (e) {
+            return error(400, "Invalid category data")
+        }
+
+        return
+    },
+}
+
+function readFormData(formData: FormData) {
+
+    const name = formData.get("name") as string
+    const color = formData.get("color") as string
+
+    return {
+        name,
+        color,
     }
 }
--- src/routes/categories/+page.svelte
+++ src/routes/categories/+page.svelte
@@ -1,7 +1,37 @@
 <script lang="ts">
 
+    import { fade } from "svelte/transition"
+    import { Category } from "@prisma/client"
+
     /**@type {import("./$types").PageData}*/
     export let data
+
+    let dialogVisible = false
+    let isEdit = false
+
+    let categoryId = -1
+
+    let name: string
+    let identifier: string
+    let color: string
+    let colorInput: HTMLInputElement
+
+    function openDialog(category?: Category) {
+
+        if (category) {
+            categoryId = category.id
+            name = category.name
+            color = category.color
+            isEdit = true
+        } else {
+            categoryId = -1
+            name = ""
+            color = "#" + Math.floor(Math.random()*16777215).toString(16) // random color code
+            isEdit = false
+        }
+
+        dialogVisible = true
+    }
 
 </script>
 
@@ -11,7 +41,7 @@
         <h1 class="text-2xl font-semibold leading-6 text-gray-900">Categories</h1>
     </div>
     <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-        <button class="btn-primary" type="button">
+        <button class="btn-primary" on:click={() => openDialog()} type="button">
             Create Category
         </button>
     </div>
@@ -31,6 +61,8 @@
                         <th class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900" scope="col">Color</th>
                         <th class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900" scope="col">Created</th>
                         <th class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900" scope="col">Updated</th>
+                        <th class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900" scope="col"></th>
+                        <th class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900" scope="col"></th>
                     </tr>
                     </thead>
                     <tbody class="divide-y divide-gray-200 bg-white">
@@ -48,6 +80,21 @@
                             </td>
                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{category.createdAt.toLocaleString()}</td>
                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{category.updatedAt.toLocaleString()}</td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                <button class="btn-text-primary" on:click={() => openDialog(category)}>
+                                    Edit
+                                </button>
+                            </td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                <form method="POST" action="?/remove">
+
+                                    <input type="hidden" id="id" name="id" value={category.id}>
+
+                                    <button type="submit" class="btn-text-primary">
+                                        Remove
+                                    </button>
+                                </form>
+                            </td>
                         </tr>
                     {/each}
 
@@ -57,3 +104,57 @@
         </div>
     </div>
 </div>
+
+
+{#if dialogVisible}
+
+    <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" transition:fade={{duration: 100}}></div>
+
+    <div class="fixed inset-0 z-10 overflow-y-auto" transition:fade={{duration: 100}}>
+        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <form
+                    method="POST" action="{ isEdit ? '?/update' : '?/create'}"
+                    class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-3xl sm:p-6">
+
+                <h1 class="text-2xl font-semibold leading-6 text-gray-900 mb-4">{ isEdit
+                    ? "Update Category"
+                    : "Create Category"}</h1>
+
+                <div class="flex flex-col space-y-2">
+
+                    <input type="hidden" name="id" value={categoryId}/>
+
+                    <div class="flex flex-row space-x-2">
+
+                        <div on:click={() => colorInput.click()}
+                             class="shape-rounded aspect-square cursor-pointer border border-black"
+                             style="background-color: {color}">
+                        </div>
+
+                        <input id="color"
+                               name="color"
+                               type="color"
+                               class="hidden"
+                               bind:value={color}
+                               bind:this={colorInput}
+                               required>
+
+                        <input id="name" name="name" class="input-text w-full" bind:value={name} required>
+
+                    </div>
+
+                </div>
+                <div class="mt-5 sm:mt-6 grid grid-flow-row-dense grid-cols-2 gap-3">
+                    <button type="button"
+                            class="btn-text-primary"
+                            on:click={() => dialogVisible = !dialogVisible}>Cancel
+                    </button>
+                    <button type="submit"
+                            class="btn-primary"
+                            on:click={() => dialogVisible = !dialogVisible}>{ isEdit ? "Update" : "Create"}
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+{/if}
