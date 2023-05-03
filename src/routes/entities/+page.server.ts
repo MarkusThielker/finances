aabc117b725de2c837331967ba0de4df9c5285ca
Markus Thielker <mail.markus.thielker@gmail.com>
2023-05-03 21:45:11 +0200

    feat(svelte): add create/edit/remove to entity

--- src/routes/entities/+page.server.ts
+++ src/routes/entities/+page.server.ts
@@ -1,7 +1,8 @@
-import type { PageServerLoad } from "./$types"
-import { redirect } from "@sveltejs/kit"
+import type { Actions, PageServerLoad } from "./$types"
+import { error, redirect } from "@sveltejs/kit"
 import { LOGIN_URL } from "$lib/constants"
 import { prismaClient } from "$lib/server/prisma"
+import type { EntityType } from "@prisma/client"
 
 export const load: PageServerLoad = async ({locals}) => {
 
@@ -17,5 +18,98 @@ export const load: PageServerLoad = async ({locals}) => {
     return {
         user,
         entities,
+    }
+}
+
+export const actions: Actions = {
+    create: async ({request, locals}) => {
+
+        const {user} = await locals.validateUser()
+        if (!user) throw redirect(302, LOGIN_URL)
+
+        const {
+            name,
+            identifier,
+            type,
+        } = readFormData(await request.formData())
+
+        console.log("After parse: ", {name, identifier, type})
+
+        try {
+            await prismaClient.entity.create({
+                data: {
+                    userId: user.userId,
+                    name,
+                    identifier,
+                    type,
+                },
+            })
+        } catch (e) {
+            throw error(400, "Invalid entity data")
+        }
+
+        return
+    },
+    update: async ({request, locals}) => {
+
+        const {user} = await locals.validateUser()
+        if (!user) throw redirect(302, LOGIN_URL)
+
+        const formData = await request.formData()
+
+        const id = Number(formData.get("id") as string)
+        if (!id) return error(400, "Invalid entity id")
+
+        const {
+            name,
+            identifier,
+            type,
+        } = readFormData(formData)
+
+        try {
+            await prismaClient.entity.update({
+                where: {id},
+                data: {
+                    name,
+                    identifier,
+                    type,
+                },
+            })
+        } catch (e) {
+            return error(400, "Invalid entity data")
+        }
+
+        return
+    },
+    remove: async ({request, locals}) => {
+
+        const {user} = await locals.validateUser()
+        if (!user) throw redirect(302, LOGIN_URL)
+
+        const formData = await request.formData()
+
+        const id = Number(formData.get("id") as string)
+        if (!id) return error(400, "Invalid entity id")
+
+        try {
+            await prismaClient.entity.delete({where: {id}})
+        } catch (e) {
+            return error(400, "Invalid entity data")
+        }
+
+        return
+    },
+}
+
+function readFormData(formData: FormData) {
+
+    const name = formData.get("name") as string
+    const identifier = formData.get("identifier") as string
+    const type = formData.get("type") as EntityType
+
+    return {
+        name,
+        identifier,
+        type,
     }
 }
--- src/routes/entities/+page.svelte
+++ src/routes/entities/+page.svelte
@@ -1,7 +1,38 @@
 <script lang="ts">
 
+    import { fade } from "svelte/transition"
+    import { Entity, EntityType } from "@prisma/client"
+
     /**@type {import("./$types").PageData}*/
     export let data
+
+    let dialogVisible = false
+    let isEdit = false
+
+    let entityId = -1
+
+    let name: string
+    let identifier: string
+    let type: EntityType
+
+    function openDialog(entity?: Entity) {
+
+        if (entity) {
+            entityId = entity.id
+            name = entity.name
+            identifier = entity.identifier
+            type = entity.type
+            isEdit = true
+        } else {
+            entityId = -1
+            name = ""
+            identifier = ""
+            type = EntityType.Entity
+            isEdit = false
+        }
+
+        dialogVisible = true
+    }
 
 </script>
 
@@ -11,7 +42,7 @@
         <h1 class="text-2xl font-semibold leading-6 text-gray-900">Entities</h1>
     </div>
     <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-        <button class="btn-primary" type="button">
+        <button class="btn-primary" on:click={() => openDialog()} type="button">
             Create Entity
         </button>
     </div>
@@ -33,6 +64,8 @@
                         <th class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900" scope="col">Type</th>
                         <th class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900" scope="col">Created</th>
                         <th class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900" scope="col">Updated</th>
+                        <th class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900" scope="col"></th>
+                        <th class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900" scope="col"></th>
                     </tr>
                     </thead>
                     <tbody class="divide-y divide-gray-200 bg-white">
@@ -46,6 +79,21 @@
                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{entity.type}</td>
                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{entity.createdAt.toLocaleString()}</td>
                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{entity.updatedAt.toLocaleString()}</td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                <button class="btn-text-primary" on:click={() => openDialog(entity)}>
+                                    Edit
+                                </button>
+                            </td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                <form method="POST" action="?/remove">
+
+                                    <input type="hidden" id="id" name="id" value={entity.id}>
+
+                                    <button type="submit" class="btn-text-primary">
+                                        Remove
+                                    </button>
+                                </form>
+                            </td>
                         </tr>
                     {/each}
 
@@ -55,3 +103,70 @@
         </div>
     </div>
 </div>
+
+
+{#if dialogVisible}
+
+    <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" transition:fade={{duration: 100}}></div>
+
+    <div class="fixed inset-0 z-10 overflow-y-auto" transition:fade={{duration: 100}}>
+        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <form
+                    method="POST" action="{ isEdit ? '?/update' : '?/create'}"
+                    class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-3xl sm:p-6">
+
+                <h1 class="text-2xl font-semibold leading-6 text-gray-900 mb-4">{ isEdit
+                    ? "Update Entity"
+                    : "Create Entity"}</h1>
+
+                <div class="flex flex-col space-y-2">
+
+                    <input type="hidden" name="id" value={entityId}/>
+
+                    <div class="flex flex-row space-x-2">
+
+                        <div class="flex flex-col w-full">
+                            <label for="name">Name</label>
+                            <input id="name" name="name" class="input-text" bind:value={name} required>
+                        </div>
+
+                    </div>
+
+                    <div class="flex flex-row space-x-2">
+
+                        <div class="flex flex-col w-full">
+                            <label for="identifier">Identifier</label>
+                            <input id="identifier" name="identifier" class="input-text" bind:value={identifier} required>
+                        </div>
+
+                    </div>
+
+                    <div class="flex flex-row space-x-2">
+
+                        <div class="flex flex-col w-full">
+                            <label for="type">Type</label>
+                            <select id="type" name="type" bind:value={type}
+                                    class="input-text w-full bg-white" required>
+                                {#each Object.values(EntityType) as entityType}
+                                    <option value={entityType.toString()}>{entityType}</option>
+                                {/each}
+                            </select>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="mt-5 sm:mt-6 grid grid-flow-row-dense grid-cols-2 gap-3">
+                    <button type="button"
+                            class="btn-text-primary"
+                            on:click={() => dialogVisible = !dialogVisible}>Cancel
+                    </button>
+                    <button type="submit"
+                            class="btn-primary"
+                            on:click={() => dialogVisible = !dialogVisible}>{ isEdit ? "Update" : "Create"}
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+{/if}
