50105693de3b047018c2be930d2ecd648af4a69c
Markus Thielker <mail.markus.thielker@gmail.com>
2023-07-03 00:17:14 +0200

    fix(svelte): set app name and remove redundant color attribute

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -40,7 +40,7 @@
                         >
                             <span class="text-black dark:text-white">{option}</span>
                             {#if option === data.scope}
-                                <span class="text-indigo-600 absolute inset-y-0 right-0 flex items-center pr-4">
+                                <span class="absolute inset-y-0 right-0 flex items-center pr-4">
                                     <svg class="h-5 w-5" viewBox="0 0 20 20" fill="#FF4500">
                                         <path fill-rule="evenodd"
                                               d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
