d758efb2e664921c642253b0bda1488197418d77
Markus Thielker <mail.markus.thielker@gmail.com>
2023-07-04 19:30:27 +0200

    fix(svelte): mobile dialogs full width

--- src/routes/categories/+page.svelte
+++ src/routes/categories/+page.svelte
@@ -119,9 +119,8 @@
 
     <div class="fixed inset-0 z-10 overflow-y-auto" transition:fade={{duration: 100}}>
         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <form
-                    method="POST" action="{ isEdit ? '?/update' : '?/create'}"
-                    class="relative transform overflow-hidden rounded-lg bg-white dark:bg-neutral-900 px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-3xl sm:p-6">
+            <form method="POST" action="{ isEdit ? '?/update' : '?/create'}"
+                  class="relative transform overflow-hidden rounded-lg bg-white dark:bg-neutral-900 px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 w-full sm:max-w-3xl sm:p-6">
 
                 <h1 class="text-2xl font-semibold leading-6 mb-4">{ isEdit
                     ? "Update Category"
